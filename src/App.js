44ed521067913c4105b017df1c2c746b6f7090e2
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2022-02-09 11:18:02 +0100

    RELATIVE DEPS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { Page, Button } from '@ks/my-local-package';
+import { Page, Button } from '@ks/how-to-develop-a-package-locally';
 
 const App = () => (
   <>
-  <Page />
-  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-    <Button
-      primary
-      label='Accept'
-      onClick={() => alert('I\'m a button from the local package!')}
-    />
-  </div>
+    <Page />
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Button
+        primary
+        label="Accept"
+        onClick={() => alert("I'm a button from the local package!")}
+      />
+    </div>
   </>
 );
 
